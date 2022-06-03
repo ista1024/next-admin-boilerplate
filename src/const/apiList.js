a483fb1dddacb57ad8fc4ad3e6b39e0f98a2eb0a
ista1024 <ista1024@gmail.com>
2022-06-03 15:57:15 +0900

    api 모듈 분리

---
+++ src/const/apiList.js
@@ -0,0 +1,23 @@
+// 로그인 기능 공통 api
+import common from "./api/common";
+// 사용자 관리 기능
+import user from "./api/user";
+// 키 관리 기능
+import key from "./api/key";
+// 인증키 관리 기능
+import apikey from "./api/apikey";
+// 이력 관리 기능
+import history from "./api/history";
+// 데이터키 관리 기능
+import datakey from "./api/datakey";
+
+const ApiList = {
+  common: common,
+  user: user,
+  key: key,
+  apikey: apikey,
+  history: history,
+  datakey: datakey,
+};
+
+export default ApiList;
