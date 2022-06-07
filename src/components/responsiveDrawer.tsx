b63b76c524e183c28734f02a99e1fc759c11a1be
ista1024 <ista1024@gmail.com>
2022-06-08 08:47:55 +0900

    add drawer & sidemenu

---
+++ src/components/responsiveDrawer.tsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import SideMenu from "@/components/sideMenu";
+import { SIDE_MENU_WIDTH } from "@/const/menu/sideMenu";
+
+import { palette } from "@mui/system";
+
+interface LayoutProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function ResponsiveDrawer({ title, children }: LayoutProps) {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [drawerWidth, setDrawerWidth] = useState(SIDE_MENU_WIDTH);
+
+  const handleDrawerToggle = () => {
+    setDrawerWidth(mobileOpen ? SIDE_MENU_WIDTH : 100);
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        color="default"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="subtitle2" noWrap component="div">
+            {title ? title : "Responsive drawer"}
+          </Typography>
+          <Typography variant="h6" noWrap component="div">
+            {title ? title : "Responsive drawer"}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      >
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          <SideMenu open={!mobileOpen} handleOpen={handleDrawerToggle} />
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open={mobileOpen}
+        >
+          <SideMenu open={!mobileOpen} handleOpen={handleDrawerToggle} />
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+}
---
+++ src/components/sideMenu.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { styled, useTheme, Theme } from "@mui/material/styles";
+import Divider from "@mui/material/Divider";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import sideMenu, {
+  hasSubMenu,
+  SideMenuConstProps,
+} from "@/const/menu/sideMenu";
+
+interface SideMenuProps {
+  open: boolean;
+  handleOpen: () => void;
+}
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+export default function SideMenu({ open, handleOpen }: SideMenuProps) {
+  const theme = useTheme();
+  const [showMenu, setMenu] = useState<boolean[]>([]);
+
+  const handleSubMenu = (index: number) => {
+    // setShowHistoryMenu(!showHistoryMenu);
+    return (
+      <div>{showMenu[index] ? <ExpandLessIcon /> : <ExpandMoreIcon />}</div>
+    );
+  };
+
+  const handleSubMenuClick = (idx: number) => {
+    const newShowMenu = [...showMenu];
+    newShowMenu[idx] = !showMenu[idx];
+    setMenu(newShowMenu);
+  };
+
+  return (
+    <div>
+      <DrawerHeader>
+        <IconButton onClick={() => handleOpen()}>
+          {!open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {sideMenu.menus.map((menu, index) => (
+          <>
+            <ListItem
+              key={menu.title}
+              onClick={() => {
+                hasSubMenu(menu) ? handleSubMenuClick(index) : null;
+              }}
+              disablePadding
+            >
+              <ListItemButton>
+                <ListItemIcon>
+                  <menu.icon />
+                </ListItemIcon>
+                {open ? <ListItemText primary={menu.title} /> : null}
+                {hasSubMenu(menu) ? handleSubMenu(index) : null}
+              </ListItemButton>
+            </ListItem>
+            {hasSubMenu(menu) ? (
+              <Collapse in={showMenu[index]} timeout="auto" unmountOnExit>
+                <List>
+                  {menu.subMenu.map((subMenu) => (
+                    <ListItem key={subMenu.title} disablePadding>
+                      <ListItemButton>
+                        <ListItemIcon></ListItemIcon>
+                        {open ? <ListItemText primary={subMenu.title} /> : null}
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+              </Collapse>
+            ) : null}
+          </>
+        ))}
+      </List>
+    </div>
+  );
+}
