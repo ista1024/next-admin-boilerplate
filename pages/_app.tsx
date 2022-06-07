4f858a464ec421e2656f4bbd9c23d669b7aeb123
ista1024 <ista1024@gmail.com>
2022-06-07 09:57:34 +0900

    delete cacheProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,15 @@
-import { CacheProvider } from "@emotion/react";
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import createEmotionCache from "@/util/createEmotionCache";
 import lightTheme from "@/styles/theme/lightTheme";
 
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-const clientSideEmotionCache = createEmotionCache();
-
-function MyApp({
-  Component,
-  emotionCache = clientSideEmotionCache,
-  pageProps,
-}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
