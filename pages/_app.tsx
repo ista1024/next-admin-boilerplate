c1bf6670eb1a4a459ad6876c8390bfe47b0ff1b0
ista1024 <ista1024@gmail.com>
2022-06-08 10:45:51 +0900

    add emotion cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,32 @@
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import * as React from "react";
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+
+import createEmotionCache from "@/util/createEmotionCache";
 import lightTheme from "@/styles/theme/lightTheme";
 import darkTheme from "@/styles/theme/darkTheme";
 
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
