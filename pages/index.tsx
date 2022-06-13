347e9f20f6ef91ba3f92da1c73eefb2e5112f3e6
ista1024 <ista1024@gmail.com>
2022-06-13 14:57:22 +0900

    install charts libraries

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,36 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
+import { Doughnut } from "react-chartjs-2";
 
 import ResponsiveDrawer from "@/components/responsiveDrawer";
 
 const Home: NextPage = () => {
+  const data = {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
   return (
     <ResponsiveDrawer title={"Dash Board"}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Dashboard!</a>
-        </h1>
         <div>
-          <Link href={"http://localhost:5500/loginHtml/login.html"} passHref>
-            <a href="http://localhost:5500/loginHtml/login.html">
-              <h1>Click here to move to Login!</h1>
-            </a>
-          </Link>
+          <a href="http://localhost:5500/loginHtml/login.html">
+            <h1>Click here to move to Login!</h1>
+          </a>
+        </div>
+        <div>
+          <Doughnut data={data} />
         </div>
       </main>
     </ResponsiveDrawer>
